585773080a906e7dbc0a80d35523e49438477299
StevenHatcher <stevenhatchersk@gmail.com>
2025-06-15 13:20:22 -0600

    Create SunsetWindow_ESP32.ino

---
+++ SunsetWindow_ESP32.ino
@@ -0,0 +1,264 @@
+#include <WiFi.h>
+#include <time.h>
+#include <HTTPClient.h>
+#include <WiFiUdp.h>
+
+#include <FastLED.h>
+#include <ArduinoJson.h>
+
+  #define LED_COUNT 69
+  #define LED_PIN 23
+
+
+// Global variables for WIFI AND API
+// ---------------------------------------------------------------------------------------------
+// Write your Wifi id and password to connect through the ESP32
+const char* ssid = "";
+const char* password = "";
+const char *ntpServer = "pool.ntp.org";
+static bool alreadyReceivedTodaysData = false; // Flag to check if the action has already been triggered at 6:00 AM
+// Coordinates
+const float LATITUDE = ;
+const float LONGITUDE = ;
+ 
+// Build the URL for the sunrisesunset API.
+String weatherURL = "https://api.sunrisesunset.io/json?lat=" + String(LATITUDE, 5) + "&lng=" + String(LONGITUDE, 5) + "&time_format=unix";
+// ---------------------------------------------------------------------------------------------
+
+// Global variables for time
+// ---------------------------------------------------------------------------------------------
+// Variable to store the current time in unix format
+
+// Variables for different important time markers through the day
+unsigned int dawn, sunrise, sunset, dusk, noon, goldenHourMorning, goldenHourNight, currentTime;
+// Floats to store the ratios of how far between important time markers are. e.g. Dawn[--------|--]Sunrise means the current time is 80% of the through the dawn-sunrise cycle
+float currentTimeProgress, goldenHourProgress;
+// const int TIME_OFFSET = -21600;
+
+// ---------------------------------------------------------------------------------------------
+
+// Global variables for the LED strip
+// ---------------------------------------------------------------------------------------------
+CRGB leds[LED_COUNT];
+unsigned int ledBrightness = 0;
+uint8_t ledColor = 255; // 0-255 as an index for the following pallete.
+// This color palette is for the transition between orange light and white light during golden hours(sunrise -> day, day -> sunset)
+DEFINE_GRADIENT_PALETTE(goldenHourColors) { 
+  0,   225,  70,  10,    // Orange at index 0
+  255, 210, 235, 250   // daylight White at index 255
+};
+CRGBPalette16 goldenHourPalette = goldenHourColors;
+// ---------------------------------------------------------------------------------------------
+
+
+
+// ---------------------------------------------------------------------------------------------
+// FUNCTIONS
+// ---------------------------------------------------------------------------------------------
+
+// Function to get the current unix time.
+unsigned int getCurrentTime() {
+  time_t now;
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    //Serial.println("Failed to obtain time");
+    return(0);
+  }
+  time(&now);
+  return now;
+}
+
+// Function to connect the ESP32 to the wifi network.
+void initWifi(){
+  WiFi.mode(WIFI_STA); // Enable station mode
+  // Open the connection to wifi
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" CONNECTED");
+  return;
+}
+
+// Function to disconnect the ESP32 from the wifi network.
+void disconnectWifi() {
+  WiFi.disconnect(false);  // Using false as a parameter so network details aren't forgotton - this increases subsequent connections when grabbing new data each day.
+  WiFi.mode(WIFI_OFF);    
+  Serial.println("Wi-Fi disconnected.");
+}
+
+void printCurrentTimeHHMMSS() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return;
+  }
+
+  char timeStr[9]; // "HH:MM:SS" + null terminator
+  strftime(timeStr, sizeof(timeStr), "%H:%M:%S", &timeinfo);
+  Serial.println(timeStr);
+}
+
+
+// This function Retrieves todays dawn, sunrise, sunset, and dusk times from https://sunrisesunset.io/api/
+// Parses the json and stores important times in global variables
+void getDateData(){
+  initWifi();
+  // Configure the time (GMT-6 offset, 0 bc no daylight savings, ntp server to get time from)
+  configTime(TIME_OFFSET, 0, ntpServer);
+  currentTime = getCurrentTime();
+
+  // Create an http client instance and make a get request to the API url.
+  HTTPClient http;
+  http.begin(weatherURL);  // This uses root CA verification (optional override available)
+  int httpCode = http.GET();
+
+  if (httpCode > 0){
+    // If the http request is successful, store the json payload 
+    String payload = http.getString();
+    // Serial.println("Received JSON:");
+    // Serial.println(payload);
+
+    // Parse JSON
+    StaticJsonDocument<2048> doc;
+    DeserializationError error = deserializeJson(doc, payload);
+    if (error){
+      Serial.print("JSON parse failed: ");
+      Serial.println(error.c_str());
+      return;
+    }
+
+    // Access values
+    JsonObject Date_Data = doc["results"];
+
+    // Set global variables to current days values
+    // currentDay = Date_Data["date"];
+    dawn = Date_Data["dawn"];
+    sunrise = Date_Data["sunrise"];
+    sunset = Date_Data["sunset"];
+    dusk = Date_Data["dusk"];
+    noon = Date_Data["solar_noon"];
+    // Set golden hour times to be 1hr away from sunrise and sunset
+    goldenHourMorning = sunrise + 3600;
+    goldenHourNight = sunset - 3600;
+  } 
+  else {
+    Serial.print("HTTP request failed: ");
+    Serial.println(http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
+  disconnectWifi();
+  return;
+}
+
+// Function to loop through the led's on the lightstrip and set their color based on the current time.
+void updateLightColor(){
+  for (int i = 0; i < LED_COUNT; i++) {
+    leds[i] = ColorFromPalette(goldenHourPalette, ledColor);
+  }
+}
+
+
+// This function calculates what time range the current time resides in (e.g. between sunrise and noon) and sets the LEDs brightness and color accordingly.
+void updateLights(){
+// if beween dawn and sunrise
+  currentTime = getCurrentTime();
+  if(currentTime >= dawn && currentTime < sunrise){
+    currentTimeProgress = float(currentTime - dawn) / float(sunrise - dawn);
+    // The current brighness will be the progress from dawn to sunrise as a percentage time the max brightness for this period (20)
+    ledBrightness = floor(currentTimeProgress * 30);
+    ledColor = 0;
+    // updateLightColor();
+  }
+  // If between sunrise and noon
+  else if(currentTime >= sunrise && currentTime < noon){
+    currentTimeProgress = float(currentTime - sunrise) / float(noon - sunrise);
+    // The brightness for this step will be the 20% brightness from sunrise, and the percentage between sunrise and noon times the difference (60)
+    ledBrightness = 30 + floor(currentTimeProgress * 50);
+    // ledColor = 255;
+    // If its golden hour (MORNING), transition the lights from orange to white
+    if(currentTime <= goldenHourMorning){
+      goldenHourProgress = float(currentTime - sunrise) / float(goldenHourMorning - sunrise);
+      ledColor = floor(goldenHourProgress * 255);
+      // updateLightColor();
+    }
+  }
+  // if between noon and sunset
+  else if(currentTime >= noon && currentTime < sunset){
+    currentTimeProgress = float(currentTime - noon) / float(sunset - noon);
+    ledBrightness = 80 - floor(currentTimeProgress * 50);
+    // If its golden hour (NIGHT), transition the lights from white to orange
+    if(currentTime >= goldenHourNight){
+      // Reducing from 100% to 0% so (1 - progress) * maxvalue.
+      goldenHourProgress = 1.0 - (float(currentTime - goldenHourNight) / float(sunset - goldenHourNight));
+      ledColor = floor(goldenHourProgress * 255);
+      // updateLightColor();
+    }
+  }
+  // if between sunset and dusk.
+  else if(currentTime >= sunset && currentTime < dusk){
+    currentTimeProgress = float(currentTime - sunset) / float(dusk - sunset);
+    ledBrightness = 30 - floor(currentTimeProgress * 30);
+    ledColor = 0;
+    // updateLightColor();
+  }
+  // All cases should be handled above, keeping this here just in case lol
+  else if(currentTime >= dusk || currentTime < dawn){ // If it's nighttime, then the lights are off and the color is preset to the base orange
+    ledBrightness = 0;
+    ledColor = 0;
+    // Serial.println("LED COLOR: ");
+    // Serial.println(ledColor);
+    // updateLightColor();
+  }
+  updateLightColor();
+  FastLED.setBrightness(ledBrightness);
+  FastLED.show();
+}
+
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+  // delay(5000);
+  
+  // Get the date data from API. Wifi connection terminates at the end of this function.
+  getDateData();
+
+  // Set up the light strip with FastLED
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, LED_COUNT);
+
+  // Alternate light colors between orange and red
+  updateLightColor();
+  // Serial.println("Current Time = ");
+  // Serial.println(currentTime);
+  updateLights();
+
+}
+
+
+void loop() {
+  // Temporary time struct for the current time. This is used to check if the time is 6:01 AM. 
+  struct tm refreshTime; 
+  if (getLocalTime(&refreshTime)) {
+    // If the current time is 6:01 AM and the data hasn't been retrieved for the new day, then we will update the date data from the API.
+    if (refreshTime.tm_hour == 1 && refreshTime.tm_min == 1) { 
+      if (!alreadyReceivedTodaysData) {
+
+        alreadyReceivedTodaysData = true;
+
+        // Get the dateData for the new day
+        initWifi();
+        getDateData();
+        disconnectWifi();
+      }
+    } else {
+      alreadyReceivedTodaysData = false;
+    }
+  }
+  updateLights();
+}
+
+
